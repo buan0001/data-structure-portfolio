e319b798b8d531a229d7b9737c05e842583e4ed4
buan0001 <bustermikkel@gmail.com>
2024-09-13 14:00:53 +0200

    Started doubly linked list

---
+++ linked-list/doubly-linked-test.js
@@ -0,0 +1,13 @@
+import DoublyLinkedList, {Node} from "./doublyLinkedList.js"
+
+
+window.addEventListener("load", start)
+
+function start(){
+    const list = new DoublyLinkedList()
+    const node1 = new Node("a")
+    console.log("node1:",node1);
+    console.log("list:",list);
+    
+    
+}
--- linked-list/doublyLinkedList.js
+++ linked-list/doublyLinkedList.js
@@ -1,1 +1,40 @@
-export default class DoublyLinkedList{}
+export default class DoublyLinkedList {
+    head;
+    tail;
+    constructor(){}
+
+    addLast( data ){}
+    addFirst( data ){}
+    get( index ){}
+    indexOf( data ){}
+    insertAfter( index, data ){}
+    insertBefore( index, data ){}
+    first(){}
+    last(){}
+    remove( data ){}
+    removeIndex( index ){}
+    removeFirst(){}
+    removeLast(){}
+
+    // Methods to work directly on nodes
+    addNodeLast( newNode ){}
+    addNodeFirst( newNode ){}
+    insertAfterNode( newNode, existingNode ){}
+    insertBeforeNode( newNode, existingNode ){}
+    removeNode( existingNode ){}
+    nodeAt( index ){}
+    swapNodes( nodeA, nodeB ){}
+
+    // Entire list methods
+    clear(){}
+    size(){}
+}
+
+export class Node {
+    previous;
+    next;
+    data;
+    constructor(data){
+        this.data = data;
+    }
+}
